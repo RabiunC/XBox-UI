aa9799adba55ce9745f15dbf734014c95d2e7463
RabiunC <fizaa99@gmail.com>
2022-12-22 12:20:53 +0530

    Implemeting data into game-tab

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/models';
 
 @Component({
   selector: 'app-game-tabs',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameTabsComponent implements OnInit {
 
-  constructor() { }
+  @Input() game: Game;
+
+  constructor() { }  
 
   ngOnInit(): void {
   }
